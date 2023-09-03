eef651210bbfae46bdba91b5909abd7af138c096
Yousef <yousefsameh24@gmail.com>
2023-09-03 13:45:58 +0300

    control delete author post req

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -119,7 +119,25 @@ exports.author_delete_get = asyncHandler(async (req, res, next) => {
 
 // Handle Author delete on POST.
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+  // Get details of author and all their books (in parallel)
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
+
+  if (allBooksByAuthor.length > 0) {
+    // Author has books. Render in same way as for GET route.
+    res.render("author_delete", {
+      title: "Delete Author",
+      author: author,
+      author_books: allBooksByAuthor,
+    });
+    return;
+  } else {
+    // Author has no books. Delete object and redirect to the list of authors.
+    await Author.findByIdAndRemove(req.body.authorid);
+    res.redirect("/catalog/authors");
+  }
 });
 
 // Display Author update form on GET.
